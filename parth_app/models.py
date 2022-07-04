82a83d30808d008d26aa1c2cba2fb6c234336e71
Pycodd123 <94187777+Pycodd123@users.noreply.github.com>
2022-07-04 12:35:53 -0700

    Upload1.5
    July4

--- parth_app/models.py
+++ parth_app/models.py
@@ -1,12 +1,3 @@
-import email
-from email import message
-from pyexpat import model
 from django.db import models
-from numpy import maximum
 
-class contactform1(models.Model):
-    fullname=models.CharField(max_length=100)
-    email=models.EmailField()
-    contact=models.CharField(max_length=15)
-    message=models.CharField(max_length=200)
-    
+# Create your models here.
--- parth_project/urls.py
+++ parth_project/urls.py
@@ -1,7 +1,7 @@
 """parth_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,13 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from cmd import PROMPT
 from django.contrib import admin
 from django.urls import path
-from django.urls import include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, path
 urlpatterns = [
     path("", include("parth_app.urls")),
     path('admin/', admin.site.urls),
+    
 ]
-urlpatterns+=staticfiles_urlpatterns()
